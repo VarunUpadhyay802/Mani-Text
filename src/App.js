1fea7f6d3b93f77520b0ea48ee699e6d2ddd336c
varun__upadhyay <varun.802.vu@gmail.com>
2023-05-10 13:49:33 +0530

    dark theme, alert and two extra functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar'
-import TextForm from './Components/TextForm';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { useState } from "react";
+import TextForm from "./Components/TextForm";
+import Alert from "./Components/Alert";
+// import About from './Components/About';
 function App() {
+  const [mode, setMode] = useState("light"); //it will tell whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null); //we are not naming below funcion to be set alert because that
+  // below functio we are using for another purpose, that show alert we are using for type and message so that
+  // through bootstarp we can get different different alert and set alert we are using only to set the alert variablr
+  //jab bhi set aata hai,means const alert , set vala set, In starting we use round brackets then inside if we want to use it for
+  // object then we can do so by curly
+  //so first alert was null, and now it is a object which is of type and msg
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has Been Enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has Been Enabled", "success");
+    }
+  };
   return (
-     <>   
-    <Navbar title={"texttutils"}/>
-    <div className='container'>
-    <TextForm heading={"Enter the text to analyze"}/>
-    </div>
-    
+    <>
+      <Navbar title={"texttutils"} mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}   />
+      <div className="container">
+        <TextForm
+          heading={"Enter the text to analyze"}
+          mode={mode}
+          toggleMode={toggleMode}
+          showAlert={showAlert}
+        />
+      </div>
     </>
-      );
+  );
 }
 
 export default App;
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const [text, setText] = useState("");
+
   const handleUpClick = () => {
     // console.log("clicked");
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("ALl the txt have been changed to the uppercase","Success")
   };
   const handleDownClick = () => {
     // console.log("clicked");
@@ -21,32 +23,76 @@ export default function TextForm(props) {
     // console.log("OnChange");
     setText(event.target.value);
   };
+  const handleCopyClick = () => {
+    var input = document.getElementById("body");
+    input.select();
+    navigator.clipboard.writeText(input.value);
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
   return (
     <>
-    <div className="mb-3">
-      <h1>{props.heading}</h1>
-      <textarea className="form-control" 
-      value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="9">
-      </textarea>
-      <button className="btn btn btn-primary" onClick={handleUpClick}>
-        Conver to UpperCase
-      </button>
-      <button className="btn btn btn-primary mx-3" onClick={handleDownClick}>
-        Conver to Downcase
-      </button>
-      <button className="btn btn btn-primary mx-3" onClick={handleClearClick}>
-        Clear Text
-      </button>
-    </div>
-    <div className='container my-3'>
-    characters:{text.length} Words:{text.split(" ").length-1}
-    <br/>
-{/* according to the data , that if you read 125 word in 30 seconds  */}
-    Avg time to read the para in seconds:{Math.floor(1.24*text.split(" ").length-1)}
-    </div>
-    Preview:
-    <br/>
-    {text}
+      <div
+        className="container"
+        style={{
+          Color: props.mode === "dark" ? "light" : "white",
+         
+        }}
+      >
+        <div className="mb-3  ">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="body"
+            rows="9"
+            style={{ backgroundColor: props.mode === "dark" ? "grey " : "white"}}
+          ></textarea>
+          <div/>
+          <button className="btn btn btn-primary" onClick={handleUpClick}>
+            Conver to UpperCase
+          </button>
+          <button
+            className="btn btn btn-primary mx-3"
+            onClick={handleDownClick}
+          >
+            Conver to Downcase
+          </button>
+          <button
+            className="btn btn btn-primary mx-3"
+            onClick={handleClearClick}
+          >
+            Clear Text
+          </button>
+          <button
+            className="btn btn btn-primary mx-3"
+            onClick={handleCopyClick}
+          >
+            Copy Text
+          </button>
+          <button
+            className="btn btn btn-primary mx-3"
+            onClick={handleExtraSpaces}
+          >
+            Remove Extra Spaces
+          </button>
+        </div>
+      </div>
+      <div className="container my-3">
+        characters:{text.length} Words:{text.split(" ").length - 1}
+        <br />
+        {/* according to the data , that if you read 125 word in 30 seconds  */}
+        Avg time to read the para in seconds:
+        {Math.floor(1.24 * text.split(" ").length - 1)}
+      </div>
+      Preview:
+      <br />
+      {text.length > 0
+        ? text
+        : "Enter something in the textbox to preview it here"}
     </>
   );
 }

4e0435fbe2563cc5ac912a17a7676fc8f9bc54ec
varun__upadhyay <varun.802.vu@gmail.com>
2023-05-11 15:50:21 +0530

    Updates

---
+++ precache-manifest.9c1418150f3748a4010477fb7af4d2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45ed38178bd8eb443f15",
+    "url": "/text-tutils/static/css/main.ee7d0c1a.chunk.css"
+  },
+  {
+    "revision": "45ed38178bd8eb443f15",
+    "url": "/text-tutils/static/js/main.45ed3817.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/text-tutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/text-tutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "faf9e4b22d20d535522b",
+    "url": "/text-tutils/static/js/runtime~main.faf9e4b2.js"
+  },
+  {
+    "revision": "cf597f332f0c2d1dfb0960323f8a357e",
+    "url": "/text-tutils/index.html"
+  }
+];
